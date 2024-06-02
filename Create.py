0180a4a80634f0e2900782a14dd84021f8ee2852
IGNACIO CORTES <icortesc@uc.cl>
2024-06-02 18:40:40 -0400

    OLBAP

--- Create.py
+++ Create.py
@@ -1,11 +1,79 @@
 import sqlite3
 import pandas as pd
+import bcrypt
+import os
 import Inserts as ins
+#e
+def BorrarTablas():
+    cursor.execute('''
+    DROP TABLE IF EXISTS Persona
+    ''')
+    conexion.commit()
+
+    cursor.execute('''
+    DROP TABLE IF EXISTS Empresa
+    ''')
+    conexion.commit()
+
+    cursor.execute('''
+    DROP TABLE IF EXISTS Despachador
+    ''')
+    conexion.commit()
+
+    cursor.execute('''
+    DROP TABLE IF EXISTS Cliente
+    ''')
+    conexion.commit()
+    
+    cursor.execute('''
+    DROP TABLE IF EXISTS Direccion
+    ''')
+    conexion.commit()
+
+    cursor.execute('''
+    DROP TABLE IF EXISTS Restaurante
+    ''')
+    conexion.commit()
+
+    cursor.execute('''
+    DROP TABLE IF EXISTS Local
+    ''')
+    conexion.commit()
+
+    cursor.execute('''
+    DROP TABLE IF EXISTS Plato
+    ''')
+    conexion.commit()
+
+    cursor.execute('''
+    DROP TABLE IF EXISTS Ingrediente
+    ''')
+    conexion.commit()
+
+    cursor.execute('''
+    DROP TABLE IF EXISTS Pedido
+    ''')
+    conexion.commit()
+
+    cursor.execute('''
+    DROP TABLE IF EXISTS DetallesPedido
+    ''')
+    conexion.commit()
+    
+    cursor.execute('''
+    DROP TABLE IF EXISTS Suscripcion
+    ''')
+    conexion.commit()
+    
+    cursor.execute('''
+    DROP DATABASE IF EXISTS Cliente
+    ''')
+    conexion.commit()
 
 def createTables(conexion, cursor):
     cursor.execute('''
     CREATE TABLE IF NOT EXISTS Persona (
-        personaID INT PRIMARY KEY,
+        personaID INTEGER PRIMARY KEY AUTOINCREMENT,
         nombre VARCHAR(255),
         numero_telefono VARCHAR(255)
     )
@@ -13,7 +81,7 @@ def createTables(conexion, cursor):
     conexion.commit()
     cursor.execute('''
     CREATE TABLE IF NOT EXISTS Empresa (
-        empresaID INT PRIMARY KEY,
+        empresaID INTEGER PRIMARY KEY AUTOINCREMENT,
         nombre VARCHAR(255),
         infoContacto VARCHAR(255)
     )
@@ -39,7 +107,7 @@ def createTables(conexion, cursor):
     conexion.commit()
     cursor.execute('''
     CREATE TABLE IF NOT EXISTS Direccion (
-        direccionID INT PRIMARY KEY,
+        direccionID INTEGER PRIMARY KEY AUTOINCREMENT,
         clienteID INT,
         direccion VARCHAR(255),
         FOREIGN KEY(clienteID) REFERENCES Cliente(clienteID)
@@ -48,16 +116,23 @@ def createTables(conexion, cursor):
     conexion.commit()
     cursor.execute('''
     CREATE TABLE IF NOT EXISTS Restaurante (
-        restauranteID INT PRIMARY KEY,
-        nombre VARCHAR(255),
-        infoContacto VARCHAR(255),
-        direccion VARCHAR(255)
+        restauranteID INTEGER PRIMARY KEY AUTOINCREMENT,
+        nombre VARCHAR(255)
     )
     ''')
     conexion.commit()
     cursor.execute('''
+    CREATE TABLE IF NOT EXISTS Local (
+        localID INTEGER PRIMARY KEY AUTOINCREMENT,
+        restauranteID INT,
+        infoContacto VARCHAR(255),
+        direccion VARCHAR(255),
+        FOREIGN KEY(restauranteID) REFERENCES Restaurante(restauranteID)
+    )
+    ''')
+    cursor.execute('''
     CREATE TABLE IF NOT EXISTS Plato (
-        platoID INT PRIMARY KEY,
+        platoID INTEGER PRIMARY KEY AUTOINCREMENT,
         nombre VARCHAR(255),
         descripcion VARCHAR(255),
         disponibilidad BOOLEAN,
@@ -72,7 +147,7 @@ def createTables(conexion, cursor):
     conexion.commit()
     cursor.execute('''
     CREATE TABLE IF NOT EXISTS Ingrediente (
-        ingredienteID INT PRIMARY KEY,
+        ingredienteID INTEGER PRIMARY KEY AUTOINCREMENT,
         nombre VARCHAR(255),
         platoID INT,
         FOREIGN KEY(platoID) REFERENCES Plato(platoID)
@@ -81,7 +156,7 @@ def createTables(conexion, cursor):
     conexion.commit()
     cursor.execute('''
     CREATE TABLE IF NOT EXISTS Pedido (
-        pedidoID INT PRIMARY KEY,
+        pedidoID INTEGER PRIMARY KEY AUTOINCREMENT,
         clienteID INT,
         despachadorID INT,
         fechaHora DATETIME,
@@ -95,8 +170,7 @@ def createTables(conexion, cursor):
     conexion.commit()
     cursor.execute('''
     CREATE TABLE IF NOT EXISTS DetallesPedido (
-        detallesPedidoID INT PRIMARY KEY,
-        empresaID INT,
+        detallesPedidoID INTEGER PRIMARY KEY AUTOINCREMENT,
         platoID INT,
         cantidad INT,
         precioTotal FLOAT,
@@ -121,9 +195,21 @@ def createTables(conexion, cursor):
     ''')
     conexion.commit()
 
-conexion = sqlite3.connect('Cliente')
+def poblarClientes(data, conexion, cursor):
+    clave = ""
+    ins.insertCliente(data[0],data[2],data[1],data[3],data[4],conexion,cursor)
+
+conexion = sqlite3.connect('grupo123e2')
 cursor = conexion.cursor()
 
 createTables(conexion, cursor)
 
-clientes_datos = pd.read_csv('datos\clientes.csv').tolist()
+path_clientes = os.path.join('datos', 'clientes.csv')
+clientes_datos = pd.read_csv(path_clientes, sep=';', header=0)
+clientes_datos = clientes_datos.values.tolist()
+for cliente in clientes_datos:
+    poblarClientes(cliente, conexion, cursor)
+    
+
+# función para agarrar todos los datos cliente
+# (nombre, numero_telefono, correo, clave, direccion, conexion, cursor)
--- Inserts.py
+++ Inserts.py
@@ -4,32 +4,33 @@ def insertPersona(nombre, numero_telefono, conexion, cursor):
 
     cursor.execute(''' 
         INSERT INTO Persona (nombre, numero_telefono) VALUES (?, ?)
-    ''', (nombre, numero_telefono))
+    ''', (nombre, numero_telefono,))
     conexion.commit()
 
 def insertCliente(nombre, numero_telefono, correo, clave, direccion, conexion, cursor):
+    cursor.execute('''
+            SELECT clienteID FROM Cliente WHERE correo = ?
+        ''', (correo,))
+    resultado = cursor.fetchone()
 
-    cursor.execute(''' 
-        SELECT COUNT(*) FROM Cliente WHERE correo = ?
-    ''', (correo,))
-    existe = cursor.fetchone()[0]
-
-    if existe != 0:
+    if not resultado:
         insertPersona(nombre, numero_telefono, conexion, cursor)
         personaID = cursor.lastrowid
 
         cursor.execute('''
-            INSTERT INTO Cliente (clienteID, correo, clave) VALUES (?, ?, ?)
-        ''', (personaID, correo, clave))
+            INSERT INTO Cliente (clienteID, correo, clave) VALUES (?, ?, ?)
+        ''', (personaID, correo, clave,))
         conexion.commit()
+    else:
+        personaID = resultado[0]
 
     insertDireccion(personaID, direccion, conexion, cursor)
 
 def insertDireccion(clienteID, direccion, conexion, cursor):
 
     cursor.execute('''
-        INSTERT INTO Direccion (clienteID, direccion) VALUES (?, ?)
-    ''', (clienteID, direccion))
+        INSERT INTO Direccion (clienteID, direccion) VALUES (?, ?)
+    ''', (clienteID, direccion,))
     conexion.commit()
 
 def insertEmpresa(nombre, infoContacto, conexion, cursor):
@@ -39,14 +40,66 @@ def insertEmpresa(nombre, infoContacto, conexion, cursor):
     ''', (nombre, infoContacto))
     conexion.commit()
 
-def insertDespachador(nombre, numero_telefono, empresaID, conexion, cursor):
+def insertDespachador(nombre, numero_telefono, empresa, conexion, cursor):
 
-    insertPersona(nombre, numero_telefono, conexion, cursor)
-    despachadorID = cursor.lastrowid
+    #AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+    return 0;
+
+def insertRestaurante(nombre, conexion, cursor): 
 
     cursor.execute(''' 
-        INSERT INTO Despachador (despachadorID, empresaID) VALUES (?, ?)
-    ''', (despachadorID, empresaID))
+        INSERT INTO  (nombre) VALUES (?)
+    ''', (nombre))
     conexion.commit()
 
-def insertRestaurante
+def insertLocal(restaurante, infoContacto, direccion, conexion, cursor):
+    cursor.execute('''
+            SELECT restauranteID FROM Restaurante WHERE nombre = ?
+        ''', (restaurante,))
+    restauranteID = cursor.fetchone()[0]
+
+    cursor.execute(''' 
+        INSERT INTO Local (restauranteID, infoContacto, direccion) VALUES (?, ?, ?)
+    ''', (restauranteID, infoContacto, direccion))
+    conexion.commit()
+
+def insertPlato(nombre, descripcion, disponibilidad, tamano, porcion, precio, tiempoPreparacion, restaurante, ingredientes, conexion, cursor):
+    cursor.execute('''
+            SELECT restauranteID FROM Restaurante WHERE nombre = ?
+        ''', (restaurante,))
+    restauranteID = cursor.fetchone()[0]
+
+    cursor.execute(''' 
+        INSERT INTO Plato (nombre, descripcion, disponibilidad, tamano, porcion, precio, tiempoPreparacion, restauranteID) VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+    ''', (nombre, descripcion, disponibilidad, tamano, porcion, precio, tiempoPreparacion, restauranteID))
+    conexion.commit()
+
+    platoID = cursor.lastrowid
+    for ingrediente in ingredientes:
+        insertIngrediente(ingrediente, platoID)
+
+def insertIngrediente(nombre, platoID, conexion, cursor):
+
+    cursor.execute(''' 
+        INSERT INTO Ingrediente (nombre, platoID) VALUES (?, ?)
+    ''', (nombre, platoID))
+    conexion.commit()
+
+def insertPedido(clienteCorreo, despachador, fechaHora, estado, evaluacionCliente, evaluacionDespachador, conexion, cursor):
+    
+    cursor.execute('''
+            SELECT clienteID FROM Cliente WHERE correo = ?
+        ''', (clienteCorreo,))
+    clienteID = cursor.fetchone()[0]
+
+    cursor.execute('''
+            SELECT despachadorID FROM Despachador WHERE nombre = ?
+        ''', (despachador,))
+    despachadorID = cursor.fetchone()[0]
+
+    cursor.execute(''' 
+        INSERT INTO Pedido (clienteID, despachadorID, fechaHora, estado, evaluacionCliente, evaluacionDespachador) VALUES (?, ?, ?, ?, ?, ?)
+    ''', (clienteID, despachadorID, fechaHora, estado, evaluacionCliente, evaluacionDespachador))
+    conexion.commit()
+
+#def insertDetallesPedido(plaotID, cantidad, )
